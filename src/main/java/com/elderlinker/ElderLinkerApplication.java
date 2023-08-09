ba7d1f688faa8332e0fd87c945480cbecf0ae0a0
RosieOh <20172207@gm.hannam.ac.kr>
2023-08-09 17:48:03 +0900

    ElderLinker 백엔드 초기 세팅

---
+++ src/main/java/com/elderlinker/ElderLinkerApplication.java
@@ -0,0 +1,13 @@
+package com.elderlinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElderLinkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElderLinkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elderlinker/ElderLinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elderlinker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElderLinkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
